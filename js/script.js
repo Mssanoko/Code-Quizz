5e3827d301026515ed32130dc31115e0e3f138b4
Leslie Lawson <lawsonjleslie@gmail.com>
2020-06-21 13:24:01 -0400

    updates question js page

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+      question: "Commonly used data types DO NOT include :",
+      answers: ["strings","booleans","alerts","number"],
+      correctAnswer: "alerts",
+    },
+    {
+      question: "The condition in ann if/else statement is enclosed within_____.",
+      answers: ["quotes","curly brackets", "parentheses", "square"],
+      correctAnswer: "parentheses",
+    },
+    {
+      question: "Arrays in Javascript can be used to store_____.",
+      answers: ["numbers and strings","other arrays","booleans","all of the above"],
+      correctAnswer: "all of the above"
+    },
+    {
+      question: "String values must be enclosed within ____ when being assigned to variables.",
+      answers: ["commas", "curly brackets", "quotes", "parentheses"],
+      correctAnswer:"quotes"
+    },
+    {
+    
+      question:"A very useful tool used during development and debugging for printing content to the debugger is:",
+      answers: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      correctAnswer: "console.log"
+    },
+    ]
